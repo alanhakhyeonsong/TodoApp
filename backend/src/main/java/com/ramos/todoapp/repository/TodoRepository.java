04a63b723e4c670880b0a9728b2d9056def0cef8
alanhakhyeonsong <songs4805@naver.com>
2022-01-08 23:04:36 +0900

    directory 수정

--- backend/src/main/java/com/ramos/todoapp/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ramos.todoapp.repository;
-
-import com.ramos.todoapp.data.entity.TodoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
-}
