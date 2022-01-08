5d4049b9d442227b3a9cf2e420d4cb052f086886
alanhakhyeonsong <songs4805@naver.com>
2022-01-08 22:59:51 +0900

    vue.js setting

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <ToolBar></ToolBar>
+    <MainView></MainView>
+  </div>
+</template>
+
+<script>
+import ToolBar from "@/components/ToolBar";
+import MainView from "@/views/MainView";
+
+export default {
+  name: 'App',
+  components: {
+    ToolBar,
+    MainView,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/views/MainView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="todoApp">
+    <h1>hello</h1>
+  </div>
+</template>
+
+<script>
+import {getTodos} from "@/api";
+
+export default {
+  name: "MainView",
+  methods: {
+    fun: function () {
+      getTodos()
+    }
+  }
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
